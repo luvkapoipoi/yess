28e03c9eb665e79511630dea62c89c8b94de8bf9
luvkapoipoi <deltaidk21@gmail.com>
2024-11-06 13:57:08 +0400

    Update Chapter 4.py

--- Chapter 4.py
+++ Chapter 4.py
@@ -30,3 +30,12 @@ print(alphabet[::2])
 numbers = (1, 2, 2, 3, 4, 4, 4, 5)
 print(numbers.count(4))
 print(numbers.index(2))
+
+#Nested Tuples
+nested =(1, 2, (3, 4), 5)
+print(nested[2][1])
+
+#Tuple membership Testing
+colors = ('red', 'green', 'blue')
+print('yellow' in colors)
+print('blue' in colors)
